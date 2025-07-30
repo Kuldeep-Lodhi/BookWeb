3a94ba2d0a30390adfcb9b8257a8c1b4aef9ea9e
Kuldeep-Lodhi <lkuldeep604@gmail.com>
2025-07-30 21:45:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import MainDiv02 from "./MainDiv02/MainDiv02"
 import MainDiv03 from "./MainDiv03/MainDiv03"
 import MainDiv04 from "./MainDiv04/MainDiv04"
 
-import MainDiv05 from "./Maindiv05/MainDiv05"
+import MainDiv05 from "./MainDiv05/MainDiv05"
 import MainDiv06 from "./MainDiv06/MainDiv06"
 import Login from "./Authentication/Login"
 import Signin from "./Authentication/Signin"
